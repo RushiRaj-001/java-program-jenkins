c5695baff93fe54e905ad56e740ae58b19b8f932
RushiRaj-001 <rushirajapure111@gmail.com>
2025-08-20 11:05:20 +0530

    second commit

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 7;
         int sum = a + b;
         System.out.println("Sum: " + sum);
+
+        System.out.println("this build is successful on " + new Date());
     }
 }
